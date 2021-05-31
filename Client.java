1ddbc2d42765e95535383993df52702467a5f407
Noelsky <66724340+Noelsky-code@users.noreply.github.com>
2021-06-01 01:45:58 +0900

    쓰레드 이용해 실시간 대화 프로그램 구현 완료 . exit에 대해서 테스트 완료 .

--- Client.java
+++ Client.java
@@ -4,7 +4,6 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
@@ -29,30 +28,76 @@ import javax.crypto.spec.IvParameterSpec;
 import java.util.Base64;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.text.SimpleDateFormat;
+
+public class Client extends Thread {
+
+    //쓰레드에서 사용할 객체들 
+    String id;
+    SecretKey secretKey;
+    IvParameterSpec iv;
+    Socket socket;
+    DataOutputStream dataOutputStream;
+    DataInputStream dataInputStream;
+
+    
+    public Client(String id,SecretKey secretkey, IvParameterSpec iv,Socket socket, DataOutputStream dataOutputStream, DataInputStream dataInputStream){
+        this.id=id;
+        this.secretKey = secretkey;
+        this.iv=iv;
+        this.socket=socket;
+        this.dataOutputStream = dataOutputStream;
+        this.dataInputStream= dataInputStream;
+    }
+
+    
+    @Override
+    public void run(){
+        if(id.equals("read")){//read 쓰레드일 경우 
+            while(true){
+
+                try{
+                    //server 메시지 복호화 후 출력
+                    String serverMessage_encrypted = dataInputStream.readUTF();
+                    String serverMessage = AES256_Decrypt(secretKey, iv, serverMessage_encrypted);
+
+                    System.out.println("[client] Received: \""+serverMessage+"\" "+get_date());
+                    System.out.println("[client] Encrypted: \""+serverMessage_encrypted+"\"");
+                    System.out.println();    
+                    if(serverMessage.equals("exit")){//서버가 exit을 보낼 경우 -> 쓰레드 종료 -> 소켓 종료 -> 에러발생 -> 모든 쓰레드 종료. 
+                        break;
+                    }
+                }catch (Exception e) {//소켓 닫혀 에러 발생시 쓰레드 종료. 
+                    break;
+                }
+            }
+        }
+        else if(id.equals("write")){//Write일 경우 
+            Scanner scanner=new Scanner(System.in);
+            
+            while(true){
+                try{
+                    //client 메시지 암호화 후 전달 
+                    String outMessage = scanner.nextLine();
+                    String outMessage_encrpyted = AES256_Encrypt(secretKey, iv, outMessage);
+                    dataOutputStream.writeUTF(outMessage_encrpyted);
+                    dataOutputStream.flush();
+                    
+                }
+                catch(Exception e){// 소켓이 닫혀 에러가 발생하면 종료시켜줌. 
+                    break;
+                }
+            
+            }
+            System.out.println("[client] Connection closed");//write 쓰레드가 제일 나중에 종료되므로 
+
+        }
 
 
-public class Client {
+    }
+
+
     public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException{
         
-        /*
-        //AES256 테스트 -> 제대로 수행됨 
-        System.out.println();
-        
-        SecretKey secretKey = secretKey_generator();
-        IvParameterSpec iv = Iv_generator();
-        
-        System.out.println(iv.getIV());
-        System.out.println(secretKey.getEncoded());
-        String in = "hansol";
-        String enc = AES256_Encrypt(secretKey,iv,in);
-        System.out.println(enc);
-        String dec = AES256_Decrypt(secretKey,iv,enc);
-        System.out.println(dec);
-        */
-
-
-
 
 
         Socket socket = null;
@@ -73,7 +118,6 @@ public class Client {
         boolean scanning = true;
         while(scanning){
             try{
-                //SocketChannel.open(new InetSocketAddress(InetAddress.getLocalHost(), 9000));
                 socket = new Socket(InetAddress.getLocalHost(), 9000);
                 scanning =false;
             }
@@ -96,126 +140,72 @@ public class Client {
         }
     
         
-       //socket.connect(new InetSocketAddress(InetAddress.getLocalHost(), 9000)); // 서버와 연결
+       //서버 연결 및 스트림 객체 생성. 
         System.out.println("[client] connected with server");
 
-        outputStream = socket.getOutputStream();
-        dataOutputStream = new DataOutputStream(outputStream);
-        objectOutputStream = new ObjectOutputStream(outputStream);
+        try{
+            outputStream = socket.getOutputStream();
+            dataOutputStream = new DataOutputStream(outputStream);
+            objectOutputStream = new ObjectOutputStream(outputStream);
     
-        inputStream = socket.getInputStream();
-        dataInputStream = new DataInputStream(inputStream);
-        objectInputStream = new ObjectInputStream(inputStream);
+            inputStream = socket.getInputStream();
+            dataInputStream = new DataInputStream(inputStream);
+            objectInputStream = new ObjectInputStream(inputStream);
 
-        //공개키 받기  
-        PublicKey publicKey = (PublicKey) objectInputStream.readObject();
-        // 공개키 출력 .. 어떻게? 
-        System.out.println();
-        System.out.println("[client] Received Public Key: " +new String(Base64.getEncoder().encode(publicKey.getEncoded()))  );
-        System.out.println();
-        //대칭키 , iv 생성
-        System.out.println("[client] Creating AES 256 KEY...");
-        SecretKey secretKey = secretKey_generator();
-        IvParameterSpec iv = Iv_generator();
-        System.out.println();
-        //대칭키 출력 .. 인코딩 
-        System.out.println("[client] AES 256 KEY : "+new String(Base64.getEncoder().encode(secretKey.getEncoded())));
-        System.out.println();
-        System.out.println("[client] iv: "+new String(Base64.getEncoder().encode(iv.getIV())));
-        System.out.println();
-        
-        //대칭키 암호화 , iv 암호화. 
-        byte[] secretKey_encrypted = RSA_Encrypt(secretKey.getEncoded(), publicKey);
-        byte[] iv_encrypted = RSA_Encrypt(iv.getIV(), publicKey);
-        //암호화된 대칭키, iv 전달. 
-        System.out.println("[client] Encrypted AES Key: "+new String(Base64.getEncoder().encode(secretKey_encrypted)));
-        System.out.println();
-        System.out.println("[client] Encrypted iv: "+ new String(Base64.getEncoder().encode(iv_encrypted)));
-        System.out.println();
-        objectOutputStream.writeObject(secretKey_encrypted);
-        objectOutputStream.flush();
-        objectOutputStream.writeObject(iv_encrypted);
-        objectOutputStream.flush();
-
-        // 키교환 완료         
-
-
-
-        //데이터 교환 
-        while(true){
-            System.out.printf(">");
-            //client 메시지 암호화 후 전달 
-            String outMessage = scanner.nextLine();
-            String outMessage_encrpyted = AES256_Encrypt(secretKey, iv, outMessage);
-            dataOutputStream.writeUTF(outMessage_encrpyted);
-            dataOutputStream.flush();
-                
-            //server 메시지 복호화 후 출력
-            String serverMessage_encrypted = dataInputStream.readUTF();
-            String serverMessage = AES256_Decrypt(secretKey, iv, serverMessage_encrypted);
+            //공개키 받기  
+            PublicKey publicKey = (PublicKey) objectInputStream.readObject();
+            // 공개키 출력 
+            System.out.println();
+            System.out.println("[client] Received Public Key: " +new String(Base64.getEncoder().encode(publicKey.getEncoded()))  );
+            System.out.println();
+            //대칭키 , iv 생성
+            System.out.println("[client] Creating AES 256 KEY...");
+            SecretKey secretKey = secretKey_generator();
+            IvParameterSpec iv = Iv_generator();
+            System.out.println();
+            //대칭키 출력 .. 인코딩 
+            System.out.println("[client] AES 256 KEY : "+new String(Base64.getEncoder().encode(secretKey.getEncoded())));
+            System.out.println();
+            System.out.println("[client] iv: "+new String(Base64.getEncoder().encode(iv.getIV())));
+            System.out.println();
             
+            //대칭키 암호화 , iv 암호화. 
+            byte[] secretKey_encrypted = RSA_Encrypt(secretKey.getEncoded(), publicKey);
+            byte[] iv_encrypted = RSA_Encrypt(iv.getIV(), publicKey);
+            //암호화된 대칭키, iv 전달. 
+            System.out.println("[client] Encrypted AES Key: "+new String(Base64.getEncoder().encode(secretKey_encrypted)));
+            System.out.println();
+            System.out.println("[client] Encrypted iv: "+ new String(Base64.getEncoder().encode(iv_encrypted)));
+            System.out.println();
+            // 대칭키교환 
+            objectOutputStream.writeObject(secretKey_encrypted);
+            objectOutputStream.flush();
+            objectOutputStream.writeObject(iv_encrypted);
+            objectOutputStream.flush();
 
-            System.out.println("[client] Received: \""+serverMessage+"\" "+get_date());
-            System.out.println("[client] Encrypted: \""+serverMessage_encrypted+"\"");    
-            if(serverMessage.equals("exit")){// 클라이언트가 exit를 보내 서버를 종료 시키고 나서 .. 
-                socket.close();
-                //System.out.println("Connection closed");
-                break;
-            }
+            //데이터 교환 파트 시작 
+
+            // read,write 쓰레드 생성
+            Client t1 = new Client("read",secretKey,iv,socket,dataOutputStream,dataInputStream);
+            Client t2 = new Client("write",secretKey,iv,socket,dataOutputStream,dataInputStream);
+            // 쓰레드 시작
+            t1.start();
+            t2.start();
+            //t1이 먼저 종료되게 설정 -> t1종료 시 socket 닫아줌 -> 모든 쓰레드 종료
+            //만약 서버에서 socket이 close 되어있어도 상관x 
+            t1.join(); 
+            socket.close();
+            if(dataOutputStream!=null)dataOutputStream.close();
+            if(outputStream!=null)outputStream.close();
+            if(dataInputStream!=null)dataInputStream.close();
+            if(inputStream!=null)inputStream.close();
+            t2.join();
         }
-
-    }
-    public static String get_date(){
-        LocalDateTime date= LocalDateTime.now();
-        //SimpleDateFormat simpl = new SimpleDateFormat("[yyyy/mm/dd/  hh:mm:ss]");
-        String s= date.format(DateTimeFormatter.ofPattern("yyyy/mm/dd hh:mm:ss"));
-        //String s= simpl.format(date);
-        return "["+s+"]";
-    }
-    public static SecretKey secretKey_generator() throws NoSuchAlgorithmException{
-        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-        SecureRandom rand = new SecureRandom();
-        keyGen.init(256,rand);
-        SecretKey secretKey = keyGen.generateKey();
-        return secretKey;
-    }
-    public static IvParameterSpec Iv_generator(){
-        SecureRandom rand = new SecureRandom();
-        byte[] iv = new byte[16];
-        rand.nextBytes(iv);
-        IvParameterSpec ivParams = new IvParameterSpec(iv);
-        return ivParams;
-
-    }
-    public static String AES256_Encrypt(SecretKey secretKey,IvParameterSpec iv,String in) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException{
-        Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        c.init(Cipher.ENCRYPT_MODE,secretKey,iv);
-        
-        byte[] encrypted = c.doFinal(in.getBytes("UTF-8"));
-        String out = new String(Base64.getEncoder().encode(encrypted));
-        return out;
-    } 
-    public static String AES256_Decrypt(SecretKey secretKey,IvParameterSpec iv,String encrypted) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException{
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");// 자바에서 PKCS5 == PKCS7
-        cipher.init(Cipher.DECRYPT_MODE,secretKey,iv);
-        byte[] decrypted = Base64.getDecoder().decode(encrypted);
-        return new String(cipher.doFinal(decrypted),"UTF-8");
-        
-    }
-    public static byte[] RSA_Encrypt(byte[] plaintext,PublicKey publicKey) throws IllegalBlockSizeException, BadPaddingException, InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException{
-        Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.ENCRYPT_MODE,publicKey);
-        byte[] encrypted = cipher.doFinal(plaintext);
-
-        return encrypted; 
-    }
-
-}
-
-
-/*
-
+        catch(Exception e){
+            e.printStackTrace();
+        }
         finally{
+            //만약 main에서 exception 발생시 stream 종료해줌. 
             try{
                 if(dataOutputStream!=null)dataOutputStream.close();
                 if(outputStream!=null)outputStream.close();
@@ -225,4 +215,56 @@ public class Client {
             catch(Exception e){
                 e.printStackTrace();
             }
-        }*/
+        }
+    }
+    //시간 정보 얻기위한 함수
+    public static String get_date(){
+        LocalDateTime date= LocalDateTime.now();
+        String s= date.format(DateTimeFormatter.ofPattern("yyyy/MM/dd hh:mm:ss"));
+
+        return "["+s+"]";
+    }
+    //대칭키 생성 함수 
+    public static SecretKey secretKey_generator() throws NoSuchAlgorithmException{
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        SecureRandom rand = new SecureRandom();
+        keyGen.init(256,rand);
+        SecretKey secretKey = keyGen.generateKey();
+        return secretKey;
+    }
+    //iv 생성 함수 
+    public static IvParameterSpec Iv_generator(){
+        SecureRandom rand = new SecureRandom();
+        byte[] iv = new byte[16];
+        rand.nextBytes(iv);
+        IvParameterSpec ivParams = new IvParameterSpec(iv);
+        return ivParams;
+
+    }
+    //aes256 암호화
+    public static String AES256_Encrypt(SecretKey secretKey,IvParameterSpec iv,String in) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException{
+        Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        c.init(Cipher.ENCRYPT_MODE,secretKey,iv);
+        
+        byte[] encrypted = c.doFinal(in.getBytes("UTF-8"));
+        String out = new String(Base64.getEncoder().encode(encrypted),"UTF-8");
+        return out;
+    } 
+    //aes256 복호화
+    public static String AES256_Decrypt(SecretKey secretKey,IvParameterSpec iv,String encrypted) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException{
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");// 자바에서 PKCS5 == PKCS7
+        cipher.init(Cipher.DECRYPT_MODE,secretKey,iv);
+        byte[] decrypted = Base64.getDecoder().decode(encrypted);
+        return new String(cipher.doFinal(decrypted),"UTF-8");
+        
+    }
+    //rsa 암호화 
+    public static byte[] RSA_Encrypt(byte[] plaintext,PublicKey publicKey) throws IllegalBlockSizeException, BadPaddingException, InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException{
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE,publicKey);
+        byte[] encrypted = cipher.doFinal(plaintext);
+
+        return encrypted; 
+    }
+
+}
--- Server.java
+++ Server.java
@@ -28,13 +28,76 @@ import java.net.InetSocketAddress;
 import java.util.Base64;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.text.SimpleDateFormat;
 
 
 
 
 
-public class Server {
+
+public class Server extends Thread{
+    
+    String id;
+    SecretKey secretKey;
+    IvParameterSpec iv;
+    Socket socket;
+    DataOutputStream dataOutputStream;
+    DataInputStream dataInputStream;
+
+    public Server(String id,SecretKey secretkey, IvParameterSpec iv,Socket socket, DataOutputStream dataOutputStream, DataInputStream dataInputStream){
+        this.id=id;
+        this.secretKey = secretkey;
+        this.iv=iv;
+        this.socket=socket;
+        this.dataOutputStream = dataOutputStream;
+        this.dataInputStream= dataInputStream;
+    }
+    @Override
+    public void run(){
+        if(id.equals("read")){//read일 경우 
+            while(true){
+
+                try{
+                    //클라이언트가 보낸 메시지 복호, 출력 
+                    String clientMessage_encrypted = dataInputStream.readUTF();
+                    String clientMessage = AES256_Decrypt(secretKey, iv, clientMessage_encrypted);
+                    System.out.println("[server] Received: \""+ clientMessage+ "\" "+get_date());
+                    System.out.println("[server] Encrypted Message: \""+ clientMessage_encrypted+"\"");
+                    
+                    //클라이언트가 exit 신호 보냈을 때 
+                    if(clientMessage.equals("exit")){
+                        break;//종료 
+                    }
+
+                } catch (Exception e) {
+                    break;
+                }
+
+            }
+        }
+        else if(id.equals("write")){//Write일 경우 
+            Scanner scanner=new Scanner(System.in);
+            
+            while(true){
+                try{
+                    //server메시지 암호화후 전달. 
+                    String outMessage = scanner.nextLine();
+                    String outMessage_encrypted= AES256_Encrypt(secretKey, iv, outMessage);;
+                    System.out.println();
+                    dataOutputStream.writeUTF(outMessage_encrypted);// 클라이언트로 보내줌 
+                    dataOutputStream.flush(); //버퍼 FLUSH
+                    
+                }
+                catch(Exception e){// 소켓이 닫혀 에러가 발생하면 종료시켜줌. 
+                    break;
+                }
+            
+            }
+            System.out.println("[server] Connection closed");//write 쓰레드가 제일 나중에 종료됨. 
+        }
+
+
+    }
+
     static public void main(String[] args ){
         
         ServerSocket serverSocket = null;
@@ -69,7 +132,7 @@ public class Server {
             dataOutputStream = new DataOutputStream(outputStream);
             objectOutputStream = new ObjectOutputStream(outputStream);
             Scanner scanner = new Scanner(System.in);
-              
+            
             //RSA 키 생성 
             System.out.println("[server] Creating RSA Key Pair ...");
             KeyPair keyPair = genRSAKeyPair();
@@ -92,50 +155,35 @@ public class Server {
             //대칭키 복호화 , iv 복호화. 
             SecretKey secretKey = new SecretKeySpec(RSA_Decrypt(secretKey_encrypted, privateKey),"AES");
             IvParameterSpec iv = new IvParameterSpec(RSA_Decrypt(iv_encrypted,privateKey));
-            //
+            //키 교환 완료 + 출력 
             System.out.println("[server] Descrypted AES Key: "+ new String(Base64.getEncoder().encode(secretKey.getEncoded())));
             System.out.println();
             System.out.println("[server] Descrpyed iv: "+new String(Base64.getEncoder().encode(iv.getIV())));
             System.out.println();
-            // 키교환 완료 
-
-
-
-            while(true){
-                //클라이언트가 보낸 메시지 복호, 출력 
-                String clientMessage_encrypted = dataInputStream.readUTF(); // 클라이언트로 부터 UTF 인코딩으로 받음
-                String clientMessage = AES256_Decrypt(secretKey, iv, clientMessage_encrypted);
-    
-                System.out.println("[server] Received: \""+ clientMessage+ "\" "+get_date());
-                System.out.println("[server] Encrypted Message: \""+ clientMessage_encrypted+"\"");
-                if(clientMessage.equals("exit")){
-                    dataOutputStream.writeUTF("exit");//클라이언트가 exit 입력 -> 클라이언트에게 exit 메시지 전달
-                    dataOutputStream.flush();
-                    socket.close();
-                    break;
-                }
-
-                //서버가 보낼 메시지 암호화 
-                String outMessage = scanner.nextLine();
-                String outMessage_encrypted = AES256_Encrypt(secretKey, iv, outMessage);
-                if(outMessage.equals("exit")){//서버가 exit 입력 -> 클라이언트에게 전달 ,종료 
-                    dataOutputStream.writeUTF("exit");
-                    //System.out.println("Connection closed");
-                    dataOutputStream.flush();
-                    socket.close();
-                    break;
-                }
-                dataOutputStream.writeUTF(outMessage_encrypted);// 클라이언트로 보내줌 
-                dataOutputStream.flush(); //버퍼 FLUSH 
-                
-
-                
-            }
-        }catch(Exception e){
+            //read, write 쓰레드 생성 
+            Server t1 = new Server("read",secretKey,iv,socket,dataOutputStream,dataInputStream);
+            Server t2 = new Server("write",secretKey,iv,socket,dataOutputStream,dataInputStream);
+            //쓰레드 시작
+            t1.start();
+            t2.start();
+            //t1 먼저 종료 => t1 종료시 socket 닫아줌 -> 모든 쓰레드 종료
+            //만약 클라이언트에서 socket이 close되었어도 상관 x 
+            t1.join();
+            socket.close();
+            if(dataOutputStream!=null)dataOutputStream.close();
+            if(outputStream!=null)outputStream.close();
+            if(dataInputStream!=null)dataInputStream.close();
+            if(inputStream!=null)inputStream.close();
+            t2.join();
+        
+        }
+        catch(Exception e){
             e.printStackTrace();
         }
         finally{
+            //만약 main에서 exception 발생시 stream 종료해줌. 
             try{
+                
                 if(dataOutputStream!=null)dataOutputStream.close();
                 if(outputStream!=null)outputStream.close();
                 if(dataInputStream!=null)dataInputStream.close();
@@ -146,13 +194,17 @@ public class Server {
             }
         }
 
+
+
+
     }
+    //날짜 정보 얻기 함수
     public static String get_date(){
         LocalDateTime date= LocalDateTime.now();
-        //SimpleDateFormat simpl = new SimpleDateFormat("[yyyy/mm/dd/  hh:mm:ss]");
         String s= date.format(DateTimeFormatter.ofPattern("yyyy/MM/dd hh:mm:ss"));
         return "["+s+"]";
     }
+    //rsa 키 생성 
     public static KeyPair genRSAKeyPair() throws NoSuchAlgorithmException{
         SecureRandom secureRandom = new SecureRandom();
         KeyPairGenerator gen = KeyPairGenerator.getInstance("RSA");
@@ -160,6 +212,7 @@ public class Server {
         KeyPair keyPair = gen.genKeyPair();
         return keyPair;
     }
+    //rsa 복호화
     public static byte[] RSA_Decrypt(byte[] encrypted,PrivateKey privateKey) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException{ 
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.DECRYPT_MODE,privateKey);
@@ -167,14 +220,16 @@ public class Server {
 
         return decrypted; 
     }
+    //aes256 암호화
     public static String AES256_Encrypt(SecretKey secretKey,IvParameterSpec iv,String in) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException{
         Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding"); // 자바에서 PKCS5 == PKCS7 
         c.init(Cipher.ENCRYPT_MODE,secretKey,iv);
         
         byte[] encrypted = c.doFinal(in.getBytes("UTF-8"));
-        String out = new String(Base64.getEncoder().encode(encrypted));
+        String out = new String(Base64.getEncoder().encode(encrypted),"UTF-8");
         return out;
-    } 
+    }
+    //aes256 복호화
     public static String AES256_Decrypt(SecretKey secretKey,IvParameterSpec iv,String encrypted) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException{
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         cipher.init(Cipher.DECRYPT_MODE,secretKey,iv);
@@ -183,3 +238,4 @@ public class Server {
         
     }
 }
+
